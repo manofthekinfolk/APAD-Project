611994a2974a4dabd108c8c0bb3fc884a81117d8
manofthekinfolk <davidkinmanc@gmail.com>
2019-07-25 11:41:27 -0500

    Initial Page

---
+++ Initial Page.py
@@ -0,0 +1,15 @@
+def menu():
+    print("What do you want to do?")
+    print()
+    inputmenu = input("Press the following numbers for certain features \n\
+            [1] Add a New User \n\
+            [2] Add a New Venue \n\
+            [3] Start an Event \n\
+            [4] Display Time Slot availability at Venue \n\
+            [5] Display All Venues in time slots \n\
+            [6] Select when you want to play ")   
+    if inputmenu == '1':
+        print('Hello')
+
+menu()
+
