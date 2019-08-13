054929d2a7659a51859032e501c000342b4828a2
hlcchu <53237211+hlcchu@users.noreply.github.com>
2019-08-13 14:27:48 -0500

    Add files via upload

---
+++ PickUpSportGit/flaskblog.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    import pymysql
+    import pymysql.cursors
+
+    con= pymysql.connect('127.0.0.1','root','password','pickupsport')
+
+    with con:
+        cur =con.cursor()
+        cur.execute("use pickupsport;")
+        cur.execute("select * from Users;")
+        p=cur.fetchall()
+        for x in p:
+            print (x)
+        id = 4
+        #id=input("please input the id you want to delete:")
+        cur.execute("delete from Users where PKUserID = %s"%(id))
+        #flash page
+
+    con.close()
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ PickUpSportGit/forms.py
@@ -0,0 +1,69 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    cpassword = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class AdduserForm(FlaskForm):
+    firstname = StringField('firstname',validators=[DataRequired()])
+    lastname = StringField('lastname',validators=[DataRequired()])
+    email = StringField('email',validators=[DataRequired(), Email()])
+    streetaddress = StringField('streetaddress',validators=[DataRequired()])
+    city = StringField('city',validators=[DataRequired()])
+    admin = StringField('admin')
+    password = PasswordField('password', validators=[DataRequired()])
+    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class AddvenueForm(FlaskForm):
+    id = StringField('id',validators=[DataRequired()])
+    venuename = StringField('venuename',validators=[DataRequired()]) 
+    eventcapacity = StringField('eventcapacity',validators=[DataRequired()])  #dc
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+class CheckForm(FlaskForm):
+    id = StringField('What is Your ID?',validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+class SearcheventForm(FlaskForm):
+    venue = StringField('Which Venue?',validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+class DeleteeventsForm(FlaskForm):
+    id = StringField('What is Event ID?',validators=[DataRequired()])
+    submit = SubmitField('Delete')
+
+class DeleteuserForm(FlaskForm):
+    userid = StringField('What is User ID?',validators=[DataRequired()])
+    submit = SubmitField('Delete')
+
+class DeletevenueForm(FlaskForm):
+    venueid = StringField('What is Venue ID?',validators=[DataRequired()])
+    submit = SubmitField('Delete')
+
+class AddvenueForm(FlaskForm):
+    venue = StringField('Venue',validators=[DataRequired()])
+    eventcapacity = StringField('Event Capacity',validators=[DataRequired()])
+    submit = SubmitField('Add')
+
+class JoineventForm(FlaskForm):
+    userid = StringField('Your ID',validators=[DataRequired()])
+    eventid = StringField('Event ID',validators=[DataRequired()])
+    submit = SubmitField('Join')    
+
+
