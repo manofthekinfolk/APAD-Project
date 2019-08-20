8c71782c0b6fde1684e7f3ce6261c8609c879e55
manofthekinfolk <davidkinmanc@gmail.com>
2019-08-20 09:35:13 -0500

    Add files via upload

--- APAD-Project-3/AP2/AndroidApp/app/src/main/java/com/example/getappengine/RegisterFragment.kt
+++ APAD-Project-3/AP2/AndroidApp/app/src/main/java/com/example/getappengine/RegisterFragment.kt
@@ -2,51 +2,140 @@
 package com.example.getappengine
 
 
+import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.register_fragment.view.*
 import okhttp3.OkHttpClient
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import org.json.JSONObject
-import okhttp3.MediaType
-import okhttp3.Request
-import okhttp3.RequestBody
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+
+
 
 //DO WE NEED A POST FUNCTION?
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : Fragment(), AnkoLogger {
 //Comment out here if you want to test run the app on Android Studio
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.register_fragment, container, false)
-
+        var sp = getActivity()?.getPreferences(Context.MODE_PRIVATE)
         val registerButton = view.findViewById<View>(R.id.register_button)
         registerButton.setOnClickListener {
             doAsync {
-
-                val emailTextField = view.findViewById<TextView>(R.id.email_text).toString()
-                val passTextField = view.findViewById<TextView>(R.id.password_text).toString()
-                val firstnameTextField = view.findViewById<TextView>(R.id.firstname_text).toString()
-                val lastnameTextField = view.findViewById<TextView>(R.id.lastname_text).toString()
-                val streetaddressTextField = view.findViewById<TextView>(R.id.streetaddress_text).toString()
-                val cityTextField = view.findViewById<TextView>(R.id.city_text).toString()
+                /*
+                val em = view.findViewById<TextView>(R.id.email_text).toString()
+                val pw = view.findViewById<TextView>(R.id.password_text).toString()
+                val fn = view.findViewById<TextView>(R.id.firstname_text).toString()
+                val ln = view.findViewById<TextView>(R.id.lastname_text).toString()
+                val sa = view.findViewById<TextView>(R.id.streetaddress_text).toString()
+                val city = view.findViewById<TextView>(R.id.city_text).toString()
                 val confirmpasswordTextField = view.findViewById<TextView>(R.id.confirmpassword_text).toString()
-                val gotresponse = RegisterAttempt(
-                    emailTextField, passTextField, firstnameTextField, lastnameTextField,
-                    streetaddressTextField, cityTextField, confirmpasswordTextField
+                val url =
+                    "https://cloudsql-pickupsport.appspot.com/adduser/" + em + "/" + fn + "/" + ln + "/" + sa + "/" + city + "/" + pw
+                */
+                var response = registerUser(
+                    view.email_text.text.toString(),
+                    view.firstname_text.text.toString(),
+                    view.lastname_text.text.toString(),
+                    view.streetaddress_text.text.toString(),
+                    view.city_text.text.toString(),
+                    view.password_text.text.toString()
                 )
-                val jsonobject = JSONObject(gotresponse)
-                if (jsonobject.get("result") == emailTextField) {(activity as NavigationHost).navigateTo(LoginFragment(), false)
+
+                print(response)
+                /*
+                //response = response?.trim()?.replace("\"", "")
+                response = response?.trim()?.replace("","/")
+                print(response)
+                uiThread {
+                    info(response)
+                    if (response == "Email is taken!") {
+                        view.error_text.text = "Email is taken! Please use another username."
+                    } else {
+                        sp?.edit()?.putString("email", view.email_text.text.toString())?.apply()
+
+                        //printing out to log
+                        info(sp?.getString("Email", ""))
+
+                        //go to a different activity if it passes login and password
+                        */
+                //(activity as NavigationHost).navigateTo(HomeFragment(), false)
+            }
+        }
+
+        (activity as NavigationHost).navigateTo(HomeFragment(), false)
+
+        /*
+        view.back_button.setOnClickListener {
+            (activity as NavigationHost).navigateTo(LoginFragment(), false)
+        }
+
+        return view
+        */
+        return view
+    }
+}
+    private fun registerUser(fn: String,ln: String,email: String,sa: String, city: String,pw: String): String? {
+        val client = OkHttpClient()
+        val request = OkHttpRequest(client)  //instantiating myOK instance
+
+        val url = "https://cloudsql-pickupsport.appspot.com/adduser"
+
+        val contents: HashMap<String, String> = HashMap<String, String>()
+        contents.put("First Name", fn)
+        contents.put("Last Name", ln)
+        contents.put("email", email)
+        contents.put("street address", sa)
+        contents.put("city", city)
+        contents.put("password", pw)
+
+        println(contents)
+
+        return request.POST(url, contents)
+
+       // print(request.POST())
+    }
+
+
+
+                /*
+                data class registerinfo(
+                    val em: String,
+                    val pw: String,
+                    val fn: String,
+                    val ln: String,
+                    val sa: String,
+                    val city: String
+                    companion object{
+
                 }
+                )
+                val json = Gson().toJson(registerinfo)
+
+                   */
+    /*
+                val client = OkHttpClient()
+                val body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json)
+                val request = Request.Builder()
+                    .header("User-Agent", "Android")
+                    .url(url)
+                    .post(body)
+                    .build()
+
+
+                val response = client.newCall(request).execute()
+                val bodystr = response.body()?.toString()
+
+                (activity as NavigationHost).navigateTo(LoginFragment(), false)
+            }
             }
 
                // val url = "https://cloudsql-pickupsport.appspot.com/register"
@@ -81,9 +170,9 @@ class RegisterFragment : Fragment() {
 //Comment out here if you want to test run the app on Android Studio
 
 
-
-private fun RegisterAttempt(fn:String,ln:String,email:String, pw: String, city:String,sa:String,cp:String): String {
-    val url = "https://cloudsql-pickupsport.appspot.com/register"
+/*
+private fun RegisterAttempt(email:String,fn:String,ln:String, sa:String, city:String,pw: String): String? {
+    val url = "https://cloudsql-pickupsport.appspot.com/register/"+email+"/"+fn+"/"+ln+"/"+sa+"/"+city+"/"+pw
     val client = OkHttpClient()
 
     val json = """
@@ -94,21 +183,34 @@ private fun RegisterAttempt(fn:String,ln:String,email:String, pw: String, city:S
             "cityTextField":"${city}",
             "StreetAddressTextField":"${sa}",
             "passTextField":"${pw}"
-            "confirmpasswordTextField":"${cp}"
             }
             """.trimIndent()
 
 
-    val body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json)
-    val request = Request.Builder()
-        .header("User-Agent", "Android")
-        .url(url)
-        .post(body)
-        .build()
 
     val response = client.newCall(request).execute()
-    val bodystr =  response.body()!!.string() // this can be consumed only once
-
+    val bodystr =  response.body()?.toString() // this can be consumed only once
     return bodystr
+    //return request
 
 }
+        */
+
+private fun registerUser(email:String,fn:String,ln:String, sa:String, city:String,pw: String): String? {
+    val client = OkHttpClient()
+    val request = OkHttpRequest(client)  //instantiating myOK instance
+
+    val url = "https://barkinapad.appspot.com/adduser/"
+
+    val contents:HashMap<String, String> = HashMap<String, String>()
+    contents.put("email", email)
+    contents.put("First Name", fn)
+    contents.put("Last Name", ln)
+    contents.put("street address", sa)
+    contents.put("city", city)
+    contents.put("password", pw)
+
+    return request.POST(url, contents)
+}
+}
+        */
