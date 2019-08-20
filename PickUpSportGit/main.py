1cc05b818364877b7c480b039cdc438abd914b0b
hlcchu <53237211+hlcchu@users.noreply.github.com>
2019-08-20 16:56:34 -0500

    Add files via upload

--- PickUpSportGit/main.py
+++ PickUpSportGit/main.py
@@ -15,7 +15,7 @@
 # [START gae_python37_cloudsql_mysql]
 import os
 
-from flask import Flask, escape, request, render_template, url_for, flash, redirect
+from flask import Flask, escape, request, render_template, url_for, flash, redirect, session,jsonify
 from forms import RegistrationForm, LoginForm, AdduserForm,AddvenueForm, CheckForm, DeleteeventsForm, AddvenueForm, SearcheventForm, JoineventForm, DeletevenueForm, DeleteuserForm
 import pymysql
 import pymysql.cursors
@@ -38,7 +38,7 @@ con = pymysql.connect(user=db_user, password=db_password,unix_socket=unix_socket
 
 
 
-@app.route('/')
+
 def main():
     # When deployed to App Engine, the `GAE_ENV` environment variable will be
     # set to `standard`
@@ -55,16 +55,11 @@ def main():
         host = '127.0.0.1'
         cnx = pymysql.connect(user=db_user, password=db_password,
                               host=host, db=db_name)
-
     with cnx.cursor() as cursor:
         cursor.execute('SELECT NOW() as now;')
         result = cursor.fetchall()
         current_time = result[0][0]
-        #id=input("User ID")
-        #cur.execute("select events.EventDescription, events.FieldName, events.TimeStart, events.TimeEnd from (events inner join UsersEvent on events.PKEventID = UsersEvent.FKEventID) inner join Users on Users.PKUserID = Userevent.FKUserID and Users.PKUserID = %s;"%(id))
-        #cursor.execute('create table test(id int primary key);')
     cnx.close()
-
     return str(current_time)
 # [END gae_python37_cloudsql_mysql]
 posts =[
@@ -81,10 +76,19 @@ posts =[
         'date_posted': 'January 1st, 2019',
     },
 ]
+
+
+
 @app.route('/')
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    if session.get('logedin') is not None:
+        return render_template('home.html', posts=posts)
+    else: 
+        session['logedin'] = None
+        session['admin'] = None
+        session['id'] = None
+        return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -113,145 +117,304 @@ def register():
 #             flash('Login Unsuccessful. Please check username and password', 'danger')
 #     return render_template('login.html', title='Login', form=form)
 @app.route("/login",methods=['GET', 'POST'])
-def login():
+@app.route('/login/<email>/<password>', methods=['GET','POST'])
+def login(email=None, password=None):
     form = LoginForm()
-    if form.validate_on_submit():
+    if email==None and password ==None:
+        if form.validate_on_submit():
+            with con:
+                cur =con.cursor()
+                em=form.email.data
+                pw=form.password.data
+                cur.execute("select PKUserID from Users where (Email, Password)=('%s','%s');"%(em,pw))
+                p=cur.fetchall()
+                session['id'] = p[0][0]
+                print(session['id'])
+                cur.execute("select Admin from Users where (Email, Password)=('%s','%s');"%(em,pw))
+                p=cur.fetchall()
+                session['admin'] = p[0][0]
+                #print(p)
+                if p == ():
+                    #print('Login Failed, please try again')
+                    flash(f'Login Failed, please try again', 'danger')
+                    return render_template('login.html',title='Login', form=form)
+                    #call current page
+                else:
+                    #print('success')
+                    flash(f'Login Success', 'success')
+                    session['logedin']= True
+                    if p[0][0]==1:
+                        return redirect(url_for('deleteuser'))
+                    else:
+                        return redirect(url_for('home'))
+                    #call another html page
+        return render_template('login.html', title='Login', form=form)
+    else:
+        #Android
         with con:
             cur =con.cursor()
-            em=form.email.data
-            pw=form.password.data
-            cur.execute("select Admin from Users where (Email, Password)=('%s','%s');"%(em,pw))
+            cur.execute("select Admin from Users where (Email, Password)=('%s','%s');"%(email,password))
             p=cur.fetchall()
-            print(p)
+            session['admin'] = p[0][0]
+            #print(p)
             if p == ():
-                print('Login Failed, please try again')
-                flash(f'Login Failed, please try again', 'danger')
                 return render_template('login.html',title='Login', form=form)
                 #call current page
             else:
-                print('success')
-                flash(f'Login Success', 'success')
+                #print('success')
+                session['logedin']= True
                 if p[0][0]==1:
                     return redirect(url_for('deleteuser'))
                 else:
                     return redirect(url_for('home'))
                 #call another html page
-    return render_template('login.html', title='Login', form=form)
+    return render_template('login.html', title = 'Login')
+
+
+@app.route("/logout",methods=['GET', 'POST'])
+def logout():
+    if session['logedin']==True:
+        session.pop('id', None)
+        session.pop('admin', None)
+        session.pop('logedin', None)
+        flash(f'Logout Success', 'success')
+    else:
+        flash(f'Do you wish to login?', 'success')
+        return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
 
 @app.route("/adduser", methods=['GET', 'POST'])
-def adduser():
+@app.route('/adduser/<fn>/<ln>/<em>/<pw>/<sa>/<ct>', methods=['GET', 'POST'])
+def adduser(fn=None, ln=None, em=None,pw=None,sa=None,ct=None):
     form = AdduserForm()
-    print(form.validate_on_submit())
-    print(form.firstname.data)
-    if form.validate_on_submit():
-        print(form.firstname.data)
+    if em==None and pw ==None:
+        if form.validate_on_submit():
+            print(form.firstname.data)
+            with con:
+                cur =con.cursor()
+                fn=form.firstname.data
+                ln=form.lastname.data
+                em=form.email.data
+                pw=form.password.data
+                sa=form.streetaddress.data
+                ct=form.city.data
+                cur.execute('insert into Users(FirstName, LastName, Email, Password, StreetAddress, City, Admin) values ("%s","%s","%s","%s","%s","%s",0)'%(fn,ln,em,pw,sa,ct))
+            flash(f'Account created for {form.firstname.data}!', 'success')
+            return redirect(url_for('home'))
+        return render_template('adduser.html', title='Add User', form=form)
+    else:
         with con:
             cur =con.cursor()
-            fn=form.firstname.data
-            ln=form.lastname.data
-            em=form.email.data
-            pw=form.password.data
-            sa=form.streetaddress.data
-            ct=form.city.data
+            print(fn,ln,em,pw,sa,ct)
             cur.execute('insert into Users(FirstName, LastName, Email, Password, StreetAddress, City, Admin) values ("%s","%s","%s","%s","%s","%s",0)'%(fn,ln,em,pw,sa,ct))
-        flash(f'Account created for {form.firstname.data}!', 'success')
         return redirect(url_for('home'))
-    return render_template('adduser.html', title='Add User', form=form)
+
+
+# @app.route("/searchevent",methods=['GET', 'POST'])
+# def searchevent():
+#     form = SearcheventForm()
+#     if form.validate_on_submit():
+#         with con:
+#             cur =con.cursor()
+#             ve=form.venue.data
+#             result = cur.execute("select * from events join Venues on PKVenueID=events.FKVenueID where FKVenueID=(select PKVenueID where Venue = '%s');"%(ve))
+#             if result >0:
+#                 userinfo = cur.fetchall()
+#                 return render_template('searchevent.html', userinfo = userinfo, form=form) 
+#     return render_template('searchevent.html',title='What Event in this Venue?', form=form)
 
 @app.route("/deleteuser", methods=['GET', 'POST'])
-def deleteuser():
+@app.route("/deleteuser/<em>", methods=['GET', 'POST'])
+def deleteuser(em=None):
     form = DeleteuserForm()
-    if form.validate_on_submit():
+    if em ==None:
+        if session['logedin']==True:
+            if session['admin']==True:
+                if form.validate_on_submit():
+                    with con:
+                        cur =con.cursor()
+                        em = form.userid.data
+                        cur.execute("delete from Users where Email = '%s'"%(em))    
+                    flash(f'User Deleted', 'success')
+                    return redirect(url_for('deleteuser'))
+                return render_template('deleteuser.html', title='Delete User', form=form)
+            else:
+                flash(f'You do not have access to this page', 'danger')
+                return redirect(url_for('home'))
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            userid = form.userid.data
-            cur.execute("delete from Users where Email = '%s'"%(userid))    
-        flash(f'User Deleted', 'success')
+            cur.execute("delete from Users where Email = '%s'"%(em))    
         return redirect(url_for('deleteuser'))
-    return render_template('deleteuser.html', title='Delete User', form=form)
+
+    
 
 
 @app.route("/addvenue", methods=['GET', 'POST'])
-def addvenue():
+@app.route('/addvenue/<vn>/<ec>', methods=['GET', 'POST'])
+def addvenue(vn=None,ec=None):
     form = AddvenueForm()
-    if form.validate_on_submit():
+    if vn==None and ec==None:
+        if session['logedin']==True:
+            if session['admin']==True:
+                if form.validate_on_submit():
+                    with con:
+                        cur =con.cursor()
+                        vn=form.venue.data
+                        ec=form.eventcapacity.data
+                        cur.execute("insert into Venues (Venue,EventCapacity) values ('%s',%s);"%(vn,ec))
+                    flash(f'Venue added for {form.venue.data}!', 'success')
+                    return redirect(url_for('deleteuser'))
+                return render_template('addvenue.html', title='Add Venue', form=form)
+            else:
+                flash(f'You do not have access to this page', 'danger')
+                return redirect(url_for('home'))
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            vn=form.venue.data
-            ec=form.eventcapacity.data
-            cur.execute("insert into Venues (Venue,EventCapacity) values ('%s',%s);"%(vn,ec))
-        flash(f'Venue added for {form.venue.data}!', 'success')
+            cur.execute("insert into Venues (Venue,EventCapacity) values ('%s',%s);"%(vn,ec))  
         return redirect(url_for('deleteuser'))
-    return render_template('addvenue.html', title='Add Venue', form=form)
+    
 
     
 @app.route("/deletevenue", methods=['GET', 'POST'])
-def deletevenue():
-    form = DeletevenueForm()
-    if form.validate_on_submit():
+@app.route('/deletevenue/<vn>', methods=['GET', 'POST'])
+def deletevenue(vn=None):
+    if vn==None:
+        form = DeletevenueForm()
+        if session['logedin']==True:
+            if session['admin']==True:
+                if form.validate_on_submit():
+                    with con:
+                        cur =con.cursor()
+                        id = form.venueid.data
+                        #cur.execute("delete from Venues where PKVenueID=('%s')"%(id)) 
+                        cur.execute("delete from Venues where Venue=('%s')"%(id))         
+                    flash(f'Venue Deleted', 'success')
+                    return redirect(url_for('deleteuser'))
+                return render_template('deletevenue.html', title='Delete Venue', form=form)
+            else:
+                flash(f'You do not have access to this page', 'danger')
+                return redirect(url_for('home'))
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            id = form.venueid.data
-            #cur.execute("delete from Venues where PKVenueID=('%s')"%(id)) 
-            cur.execute("delete from Venues where Venue=('%s')"%(id))         
-        flash(f'Venue Deleted', 'success')
+            cur.execute("delete from Venues where Venue=('%s')"%(vn))         
         return redirect(url_for('deleteuser'))
-    return render_template('deletevenue.html', title='Delete Venue', form=form)
 
 @app.route("/deleteevent", methods=['GET', 'POST'])
-def deleteevent():
+@app.route('/deleteevent/<id>', methods=['GET', 'POST'])
+def deleteevent(id=None):
     form = DeleteeventsForm()
-    if form.validate_on_submit():
+    if id==None:
+        if session['logedin']==True:
+            if session['admin']==True:           
+                if form.validate_on_submit():
+                    with con:
+                        cur =con.cursor()
+                        id = form.id.data
+                        cur.execute("delete from events where PKEventID = %s"%(id))            
+                        flash(f'Event Deleted', 'success')
+                    return redirect(url_for('deleteuser'))
+                return render_template('deleteevent.html', title='Delete Event', form=form)
+            else:
+                flash(f'You do not have access to this page', 'danger')
+                return redirect(url_for('home'))
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            id = form.id.data
             cur.execute("delete from events where PKEventID = %s"%(id))            
-            flash(f'Event Deleted', 'success')
         return redirect(url_for('deleteuser'))
-    return render_template('deleteevent.html', title='Delete Event', form=form)
 
 @app.route("/joinevent", methods=['GET', 'POST'])
-def joinevent():
+@app.route("/joinevent/<userid>/<eventid>", methods=['GET','POST'])
+def joinevent(userid=None,eventid=None):
     form = JoineventForm()
-    if form.validate_on_submit():
+    if userid==None and eventid==None:
+        if session['logedin']==True:
+            if form.validate_on_submit():
+                with con:
+                    cur =con.cursor()
+                    userid = form.userid.data
+                    eventid = form.eventid.data
+                    cur.execute("insert into UsersEvent(FKUserID, FKEventID) values (%s,%s);"%(userid,eventid))
+                flash(f'Event Joined', 'success')
+                return redirect(url_for('home'))
+            return render_template('joinevent.html', title='Join Event', form=form)
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            userid = form.userid.data
-            eventid = form.eventid.data
             cur.execute("insert into UsersEvent(FKUserID, FKEventID) values (%s,%s);"%(userid,eventid))
-        flash(f'Event Joined', 'success')
         return redirect(url_for('home'))
-    return render_template('joinevent.html', title='Join Event', form=form)
 
 @app.route("/eventhejoin",methods=['GET', 'POST'])
-def eventhejoin():
+@app.route("/eventhejoin/<id>",methods=['GET', 'POST'])
+def eventhejoin(id=None):
     form = CheckForm()
-    if form.validate_on_submit():
+    if id==None:
+        if session['logedin']==True:
+            if form.validate_on_submit():
+                with con:
+                    cur =con.cursor()
+                    id = form.id.data
+                    result = cur.execute("select FieldName, EventDescription, TimeStart, TimeEnd from(events join UsersEvent on UsersEvent.FKEventID=events.PKEventID ) where FKUserID = %s;"%(id))
+                    if result >0:
+                        userinfo = cur.fetchall()   
+                        return render_template('eventhejoin.html', userinfo = userinfo, form=form)
+            return render_template('eventhejoin.html',title='What Event I Join', form=form)
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            id = form.id.data
-            result = cur.execute("select FieldName, EventDescription, TimeStart, TimeEnd from(events join UsersEvent on UsersEvent.FKEventID=events.PKEventID ) where FKUserID = %s;"%(id))
-            if result >0:
-                userinfo = cur.fetchall()   
-                return render_template('eventhejoin.html', userinfo = userinfo, form=form)
-    return render_template('eventhejoin.html',title='What Event I Join', form=form)
-
-
+            cur.execute("select FieldName, EventDescription, TimeStart, TimeEnd from(events join UsersEvent on UsersEvent.FKEventID=events.PKEventID ) where FKUserID = %s;"%(id))
+            userinfo = cur.fetchall()
+            print(userinfo)   
+            return jsonify(userinfo)
 
 
 @app.route("/searchevent",methods=['GET', 'POST'])
-def searchevent():
+@app.route("/searchevent/<vn>")
+def searchevent(vn=None):
     form = SearcheventForm()
-    if form.validate_on_submit():
+    if vn==None:
+        if session['logedin']==True:
+            if form.validate_on_submit():
+                with con:
+                    cur =con.cursor()
+                    vn=form.venue.data
+                    result = cur.execute("select * from events join Venues on PKVenueID=events.FKVenueID where FKVenueID=(select PKVenueID where Venue = '%s');"%(vn))
+                    if result >0:
+                        userinfo = cur.fetchall()
+                        return render_template('searchevent.html', userinfo = userinfo, form=form) 
+            return render_template('searchevent.html',title='What Event in this Venue?', form=form)
+        else:
+            flash(f'Please Login First', 'success')
+            return redirect(url_for('login'))
+    else:
         with con:
             cur =con.cursor()
-            ve=form.venue.data
-            result = cur.execute("select * from events join Venues on PKVenueID=events.FKVenueID where FKVenueID=(select PKVenueID where Venue = '%s');"%(ve))
-            if result >0:
-                userinfo = cur.fetchall()
-                return render_template('searchevent.html', userinfo = userinfo, form=form) 
-    return render_template('searchevent.html',title='What Event in this Venue?', form=form)
+            cur.execute("select * from events join Venues on PKVenueID=events.FKVenueID where FKVenueID=(select PKVenueID where Venue = '%s');"%(vn))
+            userinfo = cur.fetchall()
+            return jsonify(userinfo)
+    
 
 
 
